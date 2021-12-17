dbd53d58fc141a8abad83636c76bd41ed9f7e4b5
Dan Buchholz <buchhlz2@gmail.com>
2021-12-17 12:53:20 -0800

    feat: add finality_header to LightClientUpdate

--- src/builder.rs
+++ src/builder.rs
@@ -1,4 +1,5 @@
-pub use eth2::types::{BlockHeaderData, BlockId, StateId, ChainSpec, EthSpec, GenericResponse, MainnetEthSpec, SignedBeaconBlock};
+pub use bls::PublicKeyBytes;
+pub use eth2::types::{BlockHeaderData, BlockId, StateId, ChainSpec, EthSpec, GenericResponse, MainnetEthSpec, SignedBeaconBlock, SyncAggregate, ValidatorId};
 pub use eth2::{BeaconNodeHttpClient, Error, Timeouts};
 pub use sensitive_url::SensitiveUrl;
 pub use crate::light_client_types::LightClientUpdate;
@@ -7,37 +8,64 @@ use std::sync::Arc;
 use std::time::Duration;
 
 pub struct Builder {
-    api_client: BeaconNodeHttpClient
+    api_client: BeaconNodeHttpClient,
+    settings: Settings
+}
+
+pub struct Settings {
+    slots_per_epoch: u64
 }
 
 impl Builder {
     pub fn new(node_url: &str) -> Self {
         let api_timeouts = Timeouts::set_all(Duration::new(5, 0));
         let surl = SensitiveUrl::parse(node_url).unwrap();
-        let beacon_node_http_client = BeaconNodeHttpClient::new(surl, api_timeouts);
+        let api_client = BeaconNodeHttpClient::new(surl, api_timeouts);
+        let settings = Settings {
+            slots_per_epoch: 32
+        };
         
         Self {
-            api_client: beacon_node_http_client
+            api_client,
+            settings
         }
     }
 
     pub async fn run<T: EthSpec>(&self, chain_spec: &ChainSpec) {
-        let (fork_name, signed_beacon_block) = self.api_client.get_beacon_blocks::<T>(BlockId::Head).await.unwrap().map(|d| (d.version, d.data)).unwrap();
+        let (fork_name, signed_beacon_block) = self.api_client.get_beacon_blocks::<T>(BlockId::Finalized).await.unwrap().map(|d| (d.version, d.data)).unwrap();
         let current_slot = signed_beacon_block.slot();
         let (beacon_block, _block_signature) = signed_beacon_block.clone().deconstruct();
         let current_epoch = beacon_block.epoch();
-        let current_sync_period = current_epoch.sync_committee_period(chain_spec);
-        let next_sync_committee_period = current_sync_period.unwrap() + 1;
+        let next_epoch = beacon_block.epoch() + 1;
+        let current_sync_committee_period = current_epoch.sync_committee_period(chain_spec);
+        let next_sync_committee_period = current_sync_committee_period.unwrap() + 1;
         let current_sync_committee_aggregate = beacon_block.body().sync_aggregate().unwrap();
 
-        // this API returns indeices, not pubkeys
+        // this API returns indices, not pubkeys
         // so either (1) fiture out how to get pubkeys, or (2) get/use state for sync committee data
-        // let _sync_committee = self.api_client.get_beacon_states_sync_committees(StateId::Slot(current_slot), None).await;
+        // let current_sync_committee_ids = self.api_client.get_beacon_states_sync_committees(StateId::Slot(current_slot), None).await.unwrap().data.validators;
+        // let next_sync_committee_ids = self.api_client.get_beacon_states_sync_committees(StateId::Slot(current_slot), None).await.unwrap().data.validators;
+        // let t = ValidatorId::Index(current_sync_committee_ids[0]);
+        // let t2 = ValidatorId::Index(next_sync_committee_ids[0]);
+        // let current_sync_committee_pubkeys = self.api_client.get_beacon_states_validator_id(StateId::Slot(current_slot), &t).await;
+        // let next_sync_committee_pubkeys = self.api_client.get_beacon_states_validator_id(StateId::Slot(next_epoch.start_slot(self.settings.slots_per_epoch)), &t2).await;
+
+        // let next_sync_committee = SyncAggregate {
+        //     sync_committee_bits: ,
+        //     sync_committee_signature:
+        // };
+
         let state = self.api_client.get_debug_beacon_states::<T>(StateId::Slot(current_slot)).await.unwrap().map(|d| d.data).unwrap();
         let current_sync_committee = state.current_sync_committee().unwrap().clone();
         let next_sync_committee = state.next_sync_committee().unwrap().clone();
+        let finality_checkpoints = self.api_client.get_beacon_states_finality_checkpoints(StateId::Slot(current_slot)).await.unwrap().map(|d| d.data).unwrap();
+        let finalized_epoch = finality_checkpoints.finalized.epoch;
+        let finalized_epoch_start_slot = finalized_epoch.start_slot(self.settings.slots_per_epoch);
+        let (_finality_fork_name, finality_signed_beacon_block) = self.api_client.get_beacon_blocks::<T>(BlockId::Slot(finalized_epoch_start_slot)).await.unwrap().map(|d| (d.version, d.data)).unwrap();
+        let (finality_beacon_block, _finality_block_signature) = finality_signed_beacon_block.clone().deconstruct();
 
         let header = beacon_block.block_header();
+        let finality_header = finality_beacon_block.block_header();
         let sync_committee_bits = current_sync_committee_aggregate.clone().sync_committee_bits;
         let sync_committee_signature = current_sync_committee_aggregate.clone().sync_committee_signature;
         let fork_version = chain_spec.fork_version_for_name(fork_name.unwrap());
@@ -46,7 +74,7 @@ impl Builder {
             header,
             next_sync_committee,
             // next_sync_committee_branch,
-            // finality_header,
+            finality_header: Some(finality_header),
             // finality_branch,
             sync_committee_bits,
             sync_committee_signature,
@@ -54,6 +82,8 @@ impl Builder {
         };
         
         println!("{:#?}", light_client_update);
+        println!("{:#?}", finality_checkpoints);
+        println!("{:#?}", current_epoch);
 
     }
 }
--- src/light_client_types.rs
+++ src/light_client_types.rs
@@ -1,4 +1,4 @@
-pub use eth2::types::{AggregateSignature, BeaconBlockHeader, BitVector, EthSpec, MainnetEthSpec, SyncCommittee};
+pub use eth2::types::{AggregateSignature, BeaconBlockHeader, BitVector, EthSpec, FixedVector, MainnetEthSpec, SyncCommittee};
 pub use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
@@ -7,7 +7,7 @@ pub struct LightClientUpdate<T: EthSpec> {
     pub header: BeaconBlockHeader,
     pub next_sync_committee: Arc<SyncCommittee<T>>,
     // pub next_sync_committee_branch: FixedVector<Hash256, T::SIZE_OF_VECTOR,
-    // pub finality_header: Option<BeaconBlockHeader>,
+    pub finality_header: Option<BeaconBlockHeader>,
     // pub finality_branch: Option<FixedVector<Hash256, T::SIZE_OF_VECTOR>>,
     pub sync_committee_bits: BitVector<T::SyncCommitteeSize>,
     pub sync_committee_signature: AggregateSignature,
